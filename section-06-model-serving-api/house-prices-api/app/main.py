5008278ffd58ec21981fc227e3a81e3fd02b4b8f
Zahra Jalali <zjalali@amazon.com>
2023-01-11 11:33:24 -0800

    test

--- section-06-model-serving-api/house-prices-api/app/main.py
+++ section-06-model-serving-api/house-prices-api/app/main.py
@@ -5,8 +5,8 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import HTMLResponse
 from loguru import logger
 
-from app.api import api_router
-from app.config import settings, setup_app_logging
+from api import api_router
+from config import settings, setup_app_logging
 
 # setup logging as early as possible
 setup_app_logging(config=settings)
